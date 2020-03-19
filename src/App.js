e6e463ea8f71a8856da22cfff7b64f9a92b54dfd
Stuart Gill <stuart.a.gill@gmail.com>
2020-03-19 15:08:50 -0700

    add async await axios request and pass user info obtained to state, then to props, and down to users component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { Component, Fragment } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <nav className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </nav>
     );
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Navbar = props => {
+const Navbar = ({ icon, title }) => {
   return (
     <nav className="navbar bg-primary">
       <h1>
-        <i className={props.icon}></i>
-        {props.title}
+        <i className={icon}></i>
+        {title}
       </h1>
     </nav>
   );
