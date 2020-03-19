a89baab68eff85fb44bd4b0209921dd0ad6808bb
Stuart Gill <stuart.a.gill@gmail.com>
2020-03-19 11:26:26 -0700

    working display of users with class based components, using props but no state

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 
 class UserItem extends Component {
   render() {
-    return <div>User Item</div>;
+    const { login, avatar_url, html_url } = this.props.user;
+    return (
+      <div className="card text-center">
+        <img
+          src={avatar_url}
+          alt=""
+          className="round-img"
+          style={{ width: '60px' }}
+        />
+        <h3>{login}</h3>
+        <div>
+          <a href={html_url} className="btn btn-dark btn-sm my-1">
+            See More
+          </a>
+        </div>
+      </div>
+    );
   }
 }
 
